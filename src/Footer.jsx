ee4234cf1795fb86a7c3263cf66b8fcb2607367a
Alam425 <body.flexer.627492@gmail.com>
2023-06-07 18:36:49 +0600

    third

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,16 +4,47 @@ const Footer = () => {
     return (
         <div className="">
             <footer className="footer pt-10 bg-base-200 text-base-content text-center h-36 justify-around">
-                <div>
-                    <Link className="text-xl flex gap-2 items-center">
-                        <img width={'50px'} src="https://i.ibb.co/QQQBKtZ/download.jpg" alt="download" border="0" className="rounded-xl" />
-                        <p className="font-bold text-purple-500">Toy&Toy</p>
-                    </Link>
+                <div className="text-xl flex gap-2 items-center">
+                    <img width={'50px'} src="https://i.ibb.co/QQQBKtZ/download.jpg" alt="download" border="0" className="rounded-xl" />
+                    <p className="font-bold text-sky-500">Toy&Toy</p>
                 </div>
                 <div>
                     <span className="footer-title">Contact Me</span>
-                    <p className="text-slate-600"> Phone : <Link className="font-bold text-slate-400" to="tel:01234567890">01234567890</Link></p>
-                    <img className="" src="../Group 9969.png" border="0" />
+                    <p className="text-slate-600">
+                        Phone :
+                        <Link className="font-bold text-slate-400" to="tel:01234567890">
+                            <div className="tooltip" data-tip="Phone Call">
+                                &nbsp; 01234567890
+                            </div>
+                        </Link>
+                    </p>
+                    <div className="flex justify-center items-center gap-2">
+                        <div className="tooltip" data-tip="Facebook">
+                            <Link to='https://web.facebook.com/' target="blank">
+                                <img className="w-10 h-10 rounded-lg hover:bg-slate-400 p-2 " src="https://cdn.iconscout.com/icon/free/png-512/free-facebook-1464523-1239437.png?f=avif&w=256" border="0" />
+                            </Link>
+                        </div>
+                        <div className="tooltip" data-tip="WhatsApp">
+                            <Link target="blank" to='https://www.whatsapp.com'>
+                                <img className="w-10 h-10 rounded-lg hover:bg-slate-400 p-2" src="https://cdn.iconscout.com/icon/free/png-512/free-whatsapp-2254775-1881161.png?f=avif&w=256" border="0" />
+                            </Link>
+                        </div>
+                        <div className="tooltip" data-tip="LinkedIn">
+                            <Link target="blank" to='https://www.linkedin.com/'>
+                                <img className="w-10 h-10 rounded-lg hover:bg-slate-400 p-2" src="https://cdn.iconscout.com/icon/free/png-512/free-linkedin-1464529-1239440.png?f=avif&w=256" border="0" />
+                            </Link>
+                        </div>
+                        <div className="tooltip" data-tip="Twitter">
+                            <Link target="blank" to='https://www.twitter.com/'>
+                                <img className="w-10 h-10 rounded-lg hover:bg-slate-400 p-2" src="https://cdn.iconscout.com/icon/free/png-512/free-twitter-1464537-1239448.png?f=avif&w=256" border="0" />
+                            </Link>
+                        </div>
+                        <div className="tooltip" data-tip="Instagram">
+                            <Link target="blank" to='https://www.instagram.com/'>
+                                <img className="w-10 h-10 rounded-lg hover:bg-slate-400 p-2" src="https://cdn.iconscout.com/icon/free/png-512/free-instagram-1464521-1239436.png?f=avif&w=256" border="0" />
+                            </Link>
+                        </div>
+                    </div>
                 </div>
                 <div>
                     <span className="footer-title">Address</span>
--- src/Login.jsx
+++ src/Login.jsx
@@ -3,34 +3,39 @@
 import { Link } from "react-router-dom";
 
 const Login = () => {
+// const handleLogin
     return (
         <div>
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="card flex-shrink-0 w-full h-[500px] max-w-sm shadow-2xl bg-base-100 ">
-                    <h6 className="py-10 text-6xl text-center font-bold">Log In!</h6>
+                    <h6 className="py-5 text-6xl text-center text-slate-600 font-bold">Log In!</h6>
                     <div className="card-body">
+                    <button className="btn btn-info btn-outline bottom-0">LogIn via Google</button>
+                    <span className="text-xl font-bold text-center text-slate-600">Or,</span>
+                        <form >
                         <div className="form-control pb-5">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
                             <input type="text" placeholder="email" className="input input-bordered" />
                         </div>
-                        <div className="form-control pb-5">
+                        <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
                             <input type="text" placeholder="password" className="input input-bordered" />
                             <label className="label">
-                                <div className="text-lg">
+                                <div className="text-md">
                                     New here?
-                                    <Link to='/signUp' className="text-lg no-underline font-semibold label-text-alt link link-hover"> SignUp Now.</Link>
+                                    <Link to='/signUp' className="no-underline font-semibold label-text-alt link link-hover"> SignUp Now.</Link>
                                 </div>
                             </label>
                         </div>
-                        <div className="form-control mt-6">
+                        <div className="form-control mt-2">
                             <button className="btn btn-primary btn-outline bottom-0">LogIn</button>
                         </div>
+                        </form>
                     </div>
                 </div>
                 <div className="hidden lg:block shadow-2xl">
